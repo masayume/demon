b23cf06115a19047b1e710f63d5e289da5bda13c
masayume <masayume.blog@gmail.com>
2014-07-22 15:06:29 +0200

    added querystring parameters

--- demon/mersenne-01.php
+++ demon/mersenne-01.php
@@ -1,27 +1,71 @@
 <?php
 
+// TODO
+// 1) http://www.html5rocks.com/en/tutorials/canvas/imagefilters/
+// 2) paging (using page, results querystring parameters)
+
+// REF.
+// http://superpixeltime.com/
+
+parse_str($_SERVER['QUERY_STRING'], $params);
+$master_seed	= $params['seed'];
+$page 		= $params['page'];
+$results 	= $params['results'];
+
+
 // init master seed
-mt_srand(100);
+mt_srand($master_seed);
 
 // init planet name data
 planet_ini();
 
-# echo "\n 1-100: " . myrand(1,100,0);
-# echo "\n 1-100: " . myrand(1,100,100000);
 
-	print "<html><head></head>";
-	print "<link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>";
-	print "\n<style=\"text/css\">";
-	print "\n</style><body style=\"background-color:#000000; color: #ffffff; font-family: 'Oswald', sans-serif;\">\n";
+
+echo <<< EOT
+<html><head></head>
+<link href='http://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
+<style="text/css">
+</style>
+<body style="background-color:#000000; color: #ffffff; font-family: 'Oswald', sans-serif;">
+EOT;
+
 	print "ini:" . count($planetname_ini) . "  mid:" . count($planetname_mid) ."  end:" . count($planetname_end);
 
-	print "<hr><div style=\"text-align:center;\">";
+	print "\n\n\n\n<hr><div style=\"text-align:center;\">";
+/*
 	for ($i=1; $i<=100; $i++) {
 		$planet_array = planet_gen();
 		print "\n<div class=\"planet\" style=\"display:inline-block; width:200px;\" > <img src=\"/demon/img/" . $planet_array[1] . "\" width=\"" . $planet_array[2]. "px\"><br clear=\"all\"><p><small>$i:</small> " . strtoupper($planet_array[0]) . "</p></div>"; 
 	}
+*/
+	for ($i=1; $i<=100; $i++) {
+                $planet_array = planet_gen();
+		$planet_name = strtoupper($planet_array[0]);
+		// $planet_img = "<img id=\"planet-$i\" src=\"/demon/img/" .$planet_array[1] . "\" width=\"" . $planet_array[2]. "px\">"; 
+		$planet_img 	= "<img id=\"planet-$i\" src=\"/demon/img/" .$planet_array[1] . "\" width=0 height=0 \">"; 
+		$javascript 	= jfunction();
+		$width		= $planet_array[2];
+	
+		echo <<< EOP
+<div class="planet" style="display:inline-block; width:200px;">
+$planet_img
+<br clear="all"><p><small>$i:</small> $planet_name </p>
+<canvas id="myCanvas-$i" width="200" height="200" style="border:1px solid #d3d3d3;"> 
+<script>
+var c=document.getElementById("myCanvas-$i");
+var ctx=c.getContext("2d");
+var img=document.getElementById("planet-$i");
+ctx.drawImage(img,10,10,$width,$width);
+
+runFilter('myCanvas-$i', Filters.grayscale);
+</script>
+</div>		
+
+EOP;
+	}
 
 	print "</div><hr>";
+	print "$javascript";
 	print "</body></html>";
 
 echo "\n";
@@ -30,6 +74,177 @@ exit(0);
 
 
 // = - = - = - = - = - = - = - = - = - = - = - = -
+
+function jfunction() {
+
+	$jfun = " 
+<script type=\"text/javascript\">
+        Filters = {};
+        Filters.getPixels = function(img) {
+          var c,ctx;
+          if (img.getContext) {
+            c = img;
+            try { ctx = c.getContext('2d'); } catch(e) {}
+          }
+          if (!ctx) {
+            c = this.getCanvas(img.width, img.height);
+            ctx = c.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+          }
+          return ctx.getImageData(0,0,c.width,c.height);
+        };
+
+        Filters.getCanvas = function(w,h) {
+          var c = document.createElement('canvas');
+          c.width = w;
+          c.height = h;
+          return c;
+        };
+
+        Filters.filterImage = function(filter, image, var_args) {
+          var args = [this.getPixels(image)];
+          for (var i=2; i<arguments.length; i++) {
+            args.push(arguments[i]);
+          }
+          return filter.apply(null, args);
+        };
+
+        Filters.grayscale = function(pixels, args) {
+          var d = pixels.data;
+          for (var i=0; i<d.length; i+=4) {
+            var r = d[i];
+            var g = d[i+1];
+            var b = d[i+2];
+            // CIE luminance for the RGB
+            var v = 0.2126*r + 0.7152*g + 0.0722*b;
+            d[i] = d[i+1] = d[i+2] = v
+          }
+          return pixels;
+        };
+
+        Filters.brightness = function(pixels, adjustment) {
+          var d = pixels.data;
+          for (var i=0; i<d.length; i+=4) {
+            d[i] += adjustment;
+            d[i+1] += adjustment;
+            d[i+2] += adjustment;
+          }
+          return pixels;
+        };
+
+        Filters.threshold = function(pixels, threshold) {
+          var d = pixels.data;
+          for (var i=0; i<d.length; i+=4) {
+            var r = d[i];
+            var g = d[i+1];
+            var b = d[i+2];
+            var v = (0.2126*r + 0.7152*g + 0.0722*b >= threshold) ? 255 : 0;
+            d[i] = d[i+1] = d[i+2] = v
+          }
+          return pixels;
+        };
+
+        Filters.tmpCanvas = document.createElement('canvas');
+        Filters.tmpCtx = Filters.tmpCanvas.getContext('2d');
+
+        Filters.createImageData = function(w,h) {
+          return this.tmpCtx.createImageData(w,h);
+        };
+
+        Filters.convolute = function(pixels, weights, opaque) {
+          var side = Math.round(Math.sqrt(weights.length));
+          var halfSide = Math.floor(side/2);
+
+          var src = pixels.data;
+          var sw = pixels.width;
+          var sh = pixels.height;
+
+          var w = sw;
+          var h = sh;
+          var output = Filters.createImageData(w, h);
+          var dst = output.data;
+
+          var alphaFac = opaque ? 1 : 0;
+
+          for (var y=0; y<h; y++) {
+            for (var x=0; x<w; x++) {
+              var sy = y;
+              var sx = x;
+              var dstOff = (y*w+x)*4;
+              var r=0, g=0, b=0, a=0;
+              for (var cy=0; cy<side; cy++) {
+                for (var cx=0; cx<side; cx++) {
+                  var scy = Math.min(sh-1, Math.max(0, sy + cy - halfSide));
+                  var scx = Math.min(sw-1, Math.max(0, sx + cx - halfSide));
+                  var srcOff = (scy*sw+scx)*4;
+                  var wt = weights[cy*side+cx];
+                  r += src[srcOff] * wt;
+                  g += src[srcOff+1] * wt;
+                  b += src[srcOff+2] * wt;
+                  a += src[srcOff+3] * wt;
+                }
+              }
+              dst[dstOff] = r;
+              dst[dstOff+1] = g;
+              dst[dstOff+2] = b;
+              dst[dstOff+3] = a + alphaFac*(255-a);
+            }
+          }
+          return output;
+        };
+
+        if (!window.Float32Array)
+          Float32Array = Array;
+
+        Filters.convoluteFloat32 = function(pixels, weights, opaque) {
+          var side = Math.round(Math.sqrt(weights.length));
+          var halfSide = Math.floor(side/2);
+
+          var src = pixels.data;
+          var sw = pixels.width;
+          var sh = pixels.height;
+
+          var w = sw;
+          var h = sh;
+          var output = {
+            width: w, height: h, data: new Float32Array(w*h*4)
+          };
+          var dst = output.data;
+
+          var alphaFac = opaque ? 1 : 0;
+
+          for (var y=0; y<h; y++) {
+            for (var x=0; x<w; x++) {
+              var sy = y;
+              var sx = x;
+              var dstOff = (y*w+x)*4;
+              var r=0, g=0, b=0, a=0;
+              for (var cy=0; cy<side; cy++) {
+                for (var cx=0; cx<side; cx++) {
+                  var scy = Math.min(sh-1, Math.max(0, sy + cy - halfSide));
+                  var scx = Math.min(sw-1, Math.max(0, sx + cx - halfSide));
+                  var srcOff = (scy*sw+scx)*4;
+                  var wt = weights[cy*side+cx];
+                  r += src[srcOff] * wt;
+                  g += src[srcOff+1] * wt;
+                  b += src[srcOff+2] * wt;
+                  a += src[srcOff+3] * wt;
+                }
+              }
+              dst[dstOff] = r;
+              dst[dstOff+1] = g;
+              dst[dstOff+2] = b;
+              dst[dstOff+3] = a + alphaFac*(255-a);
+            }
+          }
+          return output;
+        };
+</script>";
+
+	return $jfun;
+
+} // end function jfunction 
+
 
 function myrand ($lo, $hi, $consume) {
 
@@ -40,6 +255,7 @@ function myrand ($lo, $hi, $consume) {
 }
 
 function planet_ini() {
+
 	global $planetname_ini, $planetname_mid, $planetname_end, $planet_pic;
 	$planetname_ini = array("a","al","bel","ca","car","chal","cyl","da","dei","di","e","en","eu","e","ga","ha","har","he","i","ia","ju","ka","kal","la","le","ly","m","me","mer","mi","mne","mu","ou","pa","pan","pra","pho","pro","rhe","sa","si","ska","spon","tar","tay","the","thy","ti","ve","y");
 	$planetname_mid = array("an","bio","bo","ce","cli","cu","de","di","do","dras","e","ga","ge","ger","i","la","le","les","ly","ma","mal","me","mis","mo","nan","ni","no","o","pa","pha","pe","pi","po","ry","ro","si","so","te","the","tla","u","xi");
@@ -49,6 +265,7 @@ function planet_ini() {
 } // end function planet_ini
 
 function planet_gen() {
+
 	global $planetname_ini, $planetname_mid, $planetname_end, $planet_pic;
 	$planet_name = "";
 	$planet = array();
